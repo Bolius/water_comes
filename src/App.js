d7ab58bfa4fbf1286cf49d4396969213fcc77c7a
Benjamin <benjamin@rotendahl.dk>
2019-05-15 14:21:14 +0200

    Autocomplete address inital setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Header from './components/header.js'
 import AboutSite from './components/about.js'
 import AdressSelect from './components/address_select.js'
-
+import { Container } from 'reactstrap'
 function App() {
   return (
-    <div>
-      <Header/>
-      <AboutSite/>
-      {/* <AdressSelect/> Benjamin does this */}
-    </div>
+      <div>
+        <Header/>
+        <Container>
+          <AboutSite/>
+          <AdressSelect/>
+        </Container>
+      </div>
   );
 }
 
