7ac68ff2ec69b4adb818f8252f23bdcb9dd85205
Maria Bich Pham <bntp.aabc@gmail.com>
2019-05-22 13:45:12 +0200

    initial map component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './components/header.js'
 import AboutSite from './components/about.js'
-import AdressSelect from './components/address_select.js'
+//import AdressSelect from './components/address_select.js'
+import MapBox from './components/map_box.js'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <AboutSite/>
       {/* <AdressSelect/> Benjamin does this */}
+      <MapBox address={"Lyngbyvej 160"}/>
     </div>
   );
 }
