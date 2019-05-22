8ed0a1a233e5869c7e0891801ba2f295aa13f018
Maria Bich Pham <bntp.aabc@gmail.com>
2019-05-22 16:49:14 +0200

    Recommendation (#3)
    * initial map component
    
    * Initial recommendation view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header.js'
 import AboutSite from './components/about.js'
 import AdressSelect from './components/address_select.js'
 import GoogleApiWrapper from './components/map_box.js'
+import Recommendations from './views/recom.js'
 import { Container } from 'reactstrap'
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <AboutSite/>
           <AdressSelect/>
           <GoogleApiWrapper address={"Jarmers Plads 1, 1551 København"} />
+          <Recommendations/>
         </Container>
       </div>
   );
---
+++ src/components/recommendation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Row, Col  } from 'reactstrap';
+import '../styles/recom.css'
+
+export default class Recommendation extends React.Component {
+  render() {
+    return (
+      <div className="recom_box">
+        <Col>
+          <Row>
+            <Col sm="4">
+              <img className="crop" src={ this.props.img } alt=""/>
+            </Col>
+            <Col sm="8">
+            <h4>{ this.props.title }</h4>
+            <p> { this.props.caption } </p>
+            <a href={ this.props.link }>Læs mere her</a>
+            </Col>
+          </Row>
+        </Col>
+      </div>
+    );
+  }
+}
