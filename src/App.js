80bcb047d1955314c15ab290bbb4bdb184a50599
Benjamin <benjamin@rotendahl.dk>
2019-05-24 11:31:50 +0200

    Split flow into address and result and started address

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from './components/header.js'
-import AboutSite from './components/about.js'
-import AdressSelect from './components/address-select.js'
-import MapBox from './components/map-box.js'
-import Recommendations from './views/recom.js'
-import ActionHandler from './views/action-handler.js'
-import { Container } from 'reactstrap'
+import Main from './main.js'
 
 
 const BaseStyle = styled.div`
@@ -18,13 +13,7 @@ function App() {
   return (
       <BaseStyle>
         <Header/>
-        <Container>
-          <AboutSite/>
-          <AdressSelect/>
-          <MapBox address={"Jarmers Plads 1, 1551 København"} />
-          <ActionHandler/>
-          <Recommendations basement={false} filter={["A", "B"]} />
-        </Container>
+        <Main/>
       </BaseStyle>
   );
 }
