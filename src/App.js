f0c1a9fd47c4fa3f679ccd0cce52e008b8390975
Benjamin <benjamin@rotendahl.dk>
2019-05-24 10:53:41 +0200

    Updated Styling, removed conflicts with typo3 scheme.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from './components/header.js'
 import AboutSite from './components/about.js'
-import AdressSelect from './components/address_select.js'
-import GoogleApiWrapper from './components/map_box.js'
+import AdressSelect from './components/address-select.js'
+import MapBox from './components/map-box.js'
 import Recommendations from './views/recom.js'
 import ActionHandler from './views/action-handler.js'
 import { Container } from 'reactstrap'
@@ -21,7 +21,7 @@ function App() {
         <Container>
           <AboutSite/>
           <AdressSelect/>
-          <GoogleApiWrapper address={"Jarmers Plads 1, 1551 København"} />
+          <MapBox address={"Jarmers Plads 1, 1551 København"} />
           <ActionHandler/>
           <Recommendations/>
         </Container>
