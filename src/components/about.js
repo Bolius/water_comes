a37b1084aae7dc8717688f3912d46b10220a1830
Maria Bich Pham <bntp.aabc@gmail.com>
2019-05-31 15:48:58 +0200

    Result flow (#21)
    * initial map component
    
    * Initial recommendation view
    
    * article json
    
    * Updated articles json, logic for recommendations
    
    * Caption slicing
    
    * Updated Styling, removed conflicts with typo3 scheme.
    
    * Switch maps api to static
    
    * Text added
    
    * Split flow into address and result and started address
    
    * Result handling flow
    
    * removed console log
    
    * Address flow is working
    
    * Updated risks from Ulrik and made state resetable
    
    * Passes BBR info along
    
    * Improvements for mobile layout
    
    * Mapping over recommendations
    
    * Removed style conflicts
    
    * centering address
    
    * Styling
    
    * Mobile layout
    
    * More styling
    
    * removed warnings
    
    * Removed round edges from buttons
    
    * risk json

--- src/components/about.js
+++ src/components/about.js
@@ -6,8 +6,12 @@ export default class About extends React.Component {
   render() {
     return (
       <Row style={{padding: "10px"}}>
-        <p>Bolius har lavet en beregner, som giver dig indblik i, hvad der kan påvirke din boligs risiko
-        for oversvømmelse. Ud fra resultatet får du gode råd til, hvad du kan gøre for at sikre din bolig.</p>
+        <h5>Tjek risikoen for, at din bolig bliver oversvømmet ved skybrud eller stormflod</h5>
+
+        <p>Indtast din adresse og fåindblik i, hvad der kan påvirke din boligs risiko
+        for oversvømmelse. Ud fra resultatet får du gode råd til, hvad du kan gøre for
+        at sikre din bolig.</p>
+
       </Row>
     );
   }
