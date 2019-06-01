d3decb06b6d475ba72d34b7f41ec21b8b5200da6
Benjamin <benjamin@rotendahl.dk>
2019-06-01 12:48:18 +0200

    V 1.0 (#25)
    * Added dist file
    
    * Fixed build settingsw
    
    * Updated build method
    
    * Design done
    
    * Removed unnecessary import
    
    * Added loggin setup
    
    * Removed flod mentions
    
    * removed flod from data mentions
    
    * Added revision
    
    * Updated rel in link
    
    * Revision

--- src/components/data-basis.js
+++ src/components/data-basis.js
@@ -43,19 +43,26 @@ export default class DataBasis extends React.Component {
           <Row>
             <Col>
               <p>
-              Vi ved, at der i fremtiden kommer flere og voldsommere vejrhændelser som
-              fx skybrud. Vi ved også, at det kan være svært at forholde sig til
-              et varsel om skybrud, hvis man ikke ved, hvor udsat ens bolig er.
+              Vi ved, at der i fremtiden kommer flere og voldsommere vejrhændelser
+              som fx skybrud. Vi ved også, at det kan være svært at forholde sig
+              til et varsel om skybrud, hvis man ikke ved, hvor udsat ens bolig
+              er.
               </p>
 
-              <p>Bolius er uvildige, og vores anbefalinger hviler på offentligt tilgængelige
-              data, som vi blandt andet indhenter gennem Bygnings-og Boligregistret (BBR).
-              Du skal være opmærksom på, at de offentligedata kan være behæftet med usikkerheder.</p>
+              <p>
+              Bolius er uvildige, og vores anbefalinger hviler på offentligt
+              tilgængelige data, som vi blandt andet indhenter gennem Bygnings- og
+              Boligregistret (BBR). Du skal være opmærksom på, at de offentlige
+              data kan være behæftet med usikkerheder.
+              </p>
 
-              <p>Data kan alene anvendes til at give en overordnet vurdering. Vi anbefaler, at du
-              indhenter yderligere informationer, før du fx iværksætter tiltag på baggrund af
-              forventninger om oversvømmelser.En god ide kan være at bestille et klimatjek,
-              der tager udgangspunkt i din boligskonkrete forhold og beliggenhed.</p>
+              <p>
+                Data kan alene anvendes til at give en overordnet vurdering. Vi
+                anbefaler, at du indhenter yderligere informationer, før du fx
+                iværksætter tiltag på baggrund af forventninger om oversvømmelser.
+                En god ide kan være at bestille et klimatjek, der tager
+                udgangspunkt i din boligs konkrete forhold og beliggenhed.
+              </p>
 
               Vores data er baseret på data fra følgende kilder:
               {this.state.collapse ? <LinkList/> : ''}
