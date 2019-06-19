3e54fa8dbc16e2b1f9ee51f783d6cd8c064339d8
Benjamin <benjamin@rotendahl.dk>
2019-06-19 10:59:39 +0200

    Switched to prod backend, gotta go fast (#55)

--- src/views/result-page.js
+++ src/views/result-page.js
@@ -1,40 +1,50 @@
-import React from 'react';
-import MapBox from '../components/map-box.js'
-import Recommendations from './recom.js'
-import ActionHandler from './action-handler.js'
-
+import React from "react";
+import MapBox from "../components/map-box.js";
+import Recommendations from "./recom.js";
+import ActionHandler from "./action-handler.js";
 
 export default class ResultPage extends React.Component {
   constructor(props) {
     super(props);
     this.setActions = this.setActions.bind(this);
-    this.state = {actions: [], showRes: false};
+    this.state = { actions: [], showRes: false };
   }
 
   setActions(actions) {
-    this.setState({ actions: actions, showRes: true } );
+    this.setState({ actions: actions, showRes: true });
   }
 
   render() {
-    let dangers = this.props.dangers
+    let dangers = this.props.dangers;
 
-    if(!(dangers.high.includes('basement') || dangers.low.includes('basement'))){
-      if(this.props.address.has_basement){
-        dangers.high.includes('lavning') ? dangers.high.push('basement') : dangers.medium.push('basement');
-      }
-      else {
-        dangers.low.push('basement')
+    if (
+      !(dangers.high.includes("basement") || dangers.low.includes("basement"))
+    ) {
+      if (this.props.address.has_basement) {
+        dangers.high.includes("lavning")
+          ? dangers.high.push("basement")
+          : dangers.medium.push("basement");
+      } else {
+        dangers.low.push("basement");
       }
     }
     return (
       <div>
-      <MapBox address={ this.props.address.text } reset={this.props.reset} />
-      <ActionHandler dangers={dangers} shown={false} setActions={ this.setActions }/>
-      {!this.state.showRes?
-        <div/>:
-        <Recommendations basement={this.props.address.has_basement} filter={this.state.actions} />
-      }
-
-    </div>
-  );}
+        <MapBox address={this.props.address.text} reset={this.props.reset} />
+        <ActionHandler
+          dangers={dangers}
+          shown={false}
+          setActions={this.setActions}
+        />
+        {!this.state.showRes ? (
+          <div />
+        ) : (
+          <Recommendations
+            basement={this.props.address.has_basement}
+            filter={this.state.actions}
+          />
+        )}
+      </div>
+    );
+  }
 }
