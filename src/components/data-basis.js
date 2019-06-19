93c219752637c0b8020342be8e66d10b1ff3ca6a
Benjamin <benjamin@rotendahl.dk>
2019-06-19 11:31:04 +0200

    Data overview (#56)
    * Better data overview

--- src/components/data-basis.js
+++ src/components/data-basis.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Container as BContainer, Row as BRow, Col, Collapse} from 'reactstrap'
-import LinkList from './link-list.js'
-import '../styles/risk.css'
+import React from "react";
+import styled from "styled-components";
+import {
+  Container as BContainer,
+  Row as BRow,
+  Col,
+  Collapse
+} from "reactstrap";
+import LinkList from "./link-list.js";
+import "../styles/risk.css";
 
 const Container = styled(BContainer)`
   margin: 5px;
   margin-top: 15px;
   margin-bottom: 15px;
-  background-color: #EFF9FD
+  background-color: #eff9fd;
 `;
 
 const Row = styled(BRow)`
@@ -30,57 +35,54 @@ export default class DataBasis extends React.Component {
   }
   componentDidUpdate() {
     if (!this.state.collapse) {
-      this.scrollToBottom()
+      this.scrollToBottom();
     }
   }
 
   scrollToBottom = () => {
-    this.dataRef.current.scrollIntoView({ behavior: 'smooth', block: "end"})
-  }
+    this.dataRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+  };
 
   render() {
     return (
       <Container>
-        <Row>
-          <Col sx='6' sm={{size: '9'}}>
-             SE DATAGRUNDLAGET HER
+        <Row onClick={this.toggle} style={{ cursor: "pointer" }}>
+          <Col sx="6" sm={{ size: "9" }}>
+            SE DATAGRUNDLAGET HER
           </Col>
-          <Col sx='3' sm={{size: '3'}} className="plus-btn" onClick={this.toggle}>
-            <div style={{cursor: "pointer"}}>{this.state.collapse ? '-': '+'}</div>
+          <Col sx="3" sm={{ size: "3" }} className="plus-btn">
+            <div>{this.state.collapse ? "-" : "+"}</div>
           </Col>
         </Row>
         <Collapse isOpen={this.state.collapse}>
           <Row>
             <Col>
               <p>
-              Vi ved, at der i fremtiden kommer flere og voldsommere vejrhændelser
-              som fx skybrud. Vi ved også, at det kan være svært at forholde sig
-              til et varsel om skybrud, hvis man ikke ved, hvor udsat ens bolig
-              er.
+                Vi ved, at der i fremtiden kommer flere og voldsommere
+                vejrhændelser som fx skybrud. Vi ved også, at det kan være svært
+                at forholde sig til et varsel om skybrud, hvis man ikke ved,
+                hvor udsat ens bolig er.
               </p>
-
               <p>
-              Bolius er uvildige, og vores anbefalinger hviler på offentligt
-              tilgængelige data, som vi blandt andet indhenter gennem Bygnings- og
-              Boligregistret (BBR). Du skal være opmærksom på, at de offentlige
-              data kan være behæftet med usikkerheder.
+                Bolius er uvildige, og vores anbefalinger hviler på offentligt
+                tilgængelige data, som vi blandt andet indhenter gennem
+                Bygnings- og Boligregistret (BBR). Du skal være opmærksom på, at
+                de offentlige data kan være behæftet med usikkerheder.
               </p>
-
               <p>
                 Data kan alene anvendes til at give en overordnet vurdering. Vi
                 anbefaler, at du indhenter yderligere informationer, før du fx
-                iværksætter tiltag på baggrund af forventninger om oversvømmelser.
-                En god ide kan være at bestille et klimatjek, der tager
-                udgangspunkt i din boligs konkrete forhold og beliggenhed.
+                iværksætter tiltag på baggrund af forventninger om
+                oversvømmelser. En god ide kan være at bestille et klimatjek,
+                der tager udgangspunkt i din boligs konkrete forhold og
+                beliggenhed.
               </p>
-
               Vores data er baseret på data fra følgende kilder:
-              {this.state.collapse ? <LinkList/> : ''}
-
+              {this.state.collapse ? <LinkList /> : ""}
             </Col>
           </Row>
         </Collapse>
-        <div ref={this.dataRef}/>
+        <div ref={this.dataRef} />
       </Container>
     );
   }
