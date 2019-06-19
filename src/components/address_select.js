438f41ffe491237e399ea0b7dc2c5c345f0fbacd
Maria Bich Pham <bntp.aabc@gmail.com>
2019-06-19 12:44:42 +0200

    fix merge conflict

---
+++ src/components/address_select.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/header.css"
+
+export default class AdressSelect extends React.Component {
+  render() {
+    return (
+      <div className="header">
+          <h1>Stormflod</h1>
+          <h5>Lorem ipsum yada dada do this</h5>
+      </div>
+    );
+  }
+}
