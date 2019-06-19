37d8741df182d6ce77b091d23b998f98c6978290
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-06-19 13:14:47 +0200

    UI fixes and basement

--- src/components/address-select.js
+++ src/components/address-select.js
@@ -14,26 +14,10 @@ export default class AdressSelect extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.setModal = this.setModal.bind(this);
     this.state = {
       address: "",
-      err_modal: false,
       dawa: require("dawa-autocomplete2")
     };
-  }
-
-  setModal() {
-    this.setState({
-      err_modal: true
-    });
-  }
-
-  toggle() {
-    this.setState({
-      err_modal: !this.state.err_modal,
-      address: ""
-    });
   }
 
   handleChange(event) {
@@ -44,54 +28,46 @@ export default class AdressSelect extends React.Component {
     }));
 
     var updateRes = this.props.setAddress;
-    var updateModal = this.setModal;
     this.state.dawa.dawaAutocomplete(inputElm, {
       select: function(dawa_res) {
         async function url_to_json(url) {
-          try {
-            const response = await fetch(url, { mode: "cors" });
-            let json = await response.json();
-            var bbr_info;
+          const response = await fetch(url, { mode: "cors" });
+          let json = await response.json();
+          var bbr_info;
 
-            bbr_info = await fetch(
-              "https://ml.bolius.dk/bbr/" + encodeURI(json.adressebetegnelse)
-            );
+          bbr_info = await fetch(
+            "https://ml.bolius.dk/bbr/" + encodeURI(json.adressebetegnelse)
+          );
 
-            let bbr_json = await bbr_info.json();
+          let bbr_json = await bbr_info.json();
 
-            json["has_basement"] = bbr_json.basement_area > 0;
-            json["bbr"] = bbr_json;
-            json["x"] = json.adgangsadresse.adgangspunkt.koordinater[0];
-            json["y"] = json.adgangsadresse.adgangspunkt.koordinater[1];
-            let resp = await fetch(
-              `https://ml.bolius.dk/waterComes/${json.x}/${json.y}`
-            );
-            let dangers = await resp.json();
-            console.log("recived");
-            console.log(dangers);
-            json["dangers"] = dangers;
-            return json;
-          } catch (e) {
-            return 1;
-          }
+          json["has_basement"] = bbr_json.basement_area > 0;
+          json["appartment"] = bbr_json.type === "story";
+          json["bbr"] = bbr_json;
+          json["x"] = json.adgangsadresse.adgangspunkt.koordinater[0];
+          json["y"] = json.adgangsadresse.adgangspunkt.koordinater[1];
+          let resp = await fetch(
+            `https://ml.bolius.dk/waterComes/${json.x}/${json.y}`
+          );
+          let dangers = await resp.json();
+          console.log("recived");
+          console.log(dangers);
+          json["dangers"] = dangers;
+          return json;
         }
 
         url_to_json(dawa_res.data.href).then(data => {
-          if (data !== 1) {
-            console.log(data);
-            let res = {
-              text: data.adressebetegnelse,
-              has_basement: data.has_basement,
-              bolig: data.adgangsadresse.bebyggelser[0],
-              bbr: data["bbr"],
-              x: data.adgangsadresse.adgangspunkt.koordinater[0],
-              y: data.adgangsadresse.adgangspunkt.koordinater[1],
-              dangers: data["dangers"]
-            };
-            updateRes(res);
-          } else {
-            updateModal();
-          }
+          console.log(data);
+          let res = {
+            text: data.adressebetegnelse,
+            has_basement: data.has_basement,
+            bolig: data.adgangsadresse.bebyggelser[0],
+            bbr: data["bbr"],
+            x: data.adgangsadresse.adgangspunkt.koordinater[0],
+            y: data.adgangsadresse.adgangspunkt.koordinater[1],
+            dangers: data["dangers"]
+          };
+          updateRes(res);
         });
       }
     });
@@ -100,76 +76,6 @@ export default class AdressSelect extends React.Component {
   render() {
     return (
       <div>
-        <Modal
-          showCloseIcon={false}
-          isOpen={this.state.err_modal}
-          closeOnEsc
-          onClose={this.toggle}
-          closeOnOverlayClick={true}
-          styles={{
-            modal: {
-              borderRadius: "30px",
-              padding: "0",
-              backgroundColor: "#404040",
-              textAlign: "center",
-              position: "relative"
-            }
-          }}
-        >
-          <div>
-            <div
-              style={{
-                border: "2px solid white",
-                height: "3em",
-                backgroundColor: "#93CBBD",
-                borderRadius: "30px",
-                textAlign: "center",
-                position: "relative",
-                zIndex: "1"
-              }}
-            >
-              <br />
-              <span className="text-danger text-center">Fejl: </span>
-              Mangelfuld data fra BBR
-            </div>
-            <div
-              style={{
-                backgroundColor: "#EBF5F5",
-                width: "100%",
-                margin: "auto",
-                marginTop: "-10px",
-                borderRadius: "20px",
-                paddingTop: "20px",
-                zIndex: "0"
-              }}
-            >
-              BBR registret indeholder ikke nok data om huset til at lave
-              forudsigelser.
-              <div style={{ padding: "10px" }}>
-                <div
-                  style={{ verticalAlign: "middle", display: "inline-block" }}
-                >
-                  <button
-                    className="comfortscore-btn"
-                    style={{ marginRight: "20px" }}
-                    onClick={() => {
-                      window.open("https://bbr.dk/ret");
-                    }}
-                  >
-                    Ret BBR data
-                  </button>
-                </div>
-                <div
-                  style={{ verticalAlign: "middle", display: "inline-block" }}
-                >
-                  <button className="comfortscore-btn" onClick={this.toggle}>
-                    Prøv anden adresse
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Modal>
         <InputBox>
           <form
             onSubmit={e => {
