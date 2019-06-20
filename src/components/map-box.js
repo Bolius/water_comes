1397cdfb7a202d4f7470b46d4ee4c66c48597006
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-06-21 00:56:46 +0200

    Removed imports

--- src/components/map-box.js
+++ src/components/map-box.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
 import { Button } from "./button.js";
 
 export default class MapBox extends React.Component {
--- src/components/risk-describer.js
+++ src/components/risk-describer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import Risk from "./risk.js";
 import Risks from "../risks.json";
 
