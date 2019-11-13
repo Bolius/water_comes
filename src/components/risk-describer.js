2760580e259ccd60aae9ba641b4363e7a5ac0287
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-11-13 15:27:53 +0100

    Clean up done

--- src/components/risk-describer.js
+++ src/components/risk-describer.js
@@ -1,63 +1,84 @@
-import React from 'react';
-import Risk from './risk.js';
-import Risks from '../risks.json';
-import { Row } from 'reactstrap';
-
+import React from "react";
+import Risk from "./risk.js";
+import Risks from "../risks.json";
+import { Row, Col } from "reactstrap";
 
 export default class RiskDescriber extends React.Component {
-  getRisks(type){
-    return this.props.dangers[type].map((id, i) =>(
-      <Risk key={i} title={Risks[id][type]} description={Risks[id].description} image={this.props.dangers[id]} percentage={this.props.dangers[String(id).concat("_percentage")]} text={Risks[id].text}/>
-    ))
+  constructor(props) {
+    super(props);
+    this.toggleTab = this.toggleTab.bind(this);
+    this.state = {
+      tab: "skybrud"
+    };
+  }
+
+  toggleTab(state) {
+    this.setState({
+      tab: state
+    });
+  }
+
+  getRisks(type) {
+    return this.props.dangers.risks[type].map((id, i) => (
+      <Risk
+        key={i}
+        title={Risks[id][type]}
+        description={Risks[id].description}
+        image={this.props.dangers[id]}
+        percentage={this.props.dangers[String(id).concat("_percentage")]}
+        text={Risks[id].text}
+      />
+    ));
   }
 
   render() {
-    const riskImageTab1 = require(`../assets/gauges/risk-${this.props.risk}.png`)
-    const riskImageTab2 = require(`../assets/gauges/risk-${this.props.risk_stormflod}.png`)
-    const base64str = this.props.dangers['stormflod']
+    const rain_image = require(`../assets/gauges/risk-${this.props.rain_risk.factor}.png`);
+    const riskImageTab2 = require(`../assets/gauges/risk-${this.props.flood_risk.factor}.png`);
 
-    var tab1 = this.props.tab1
-    var tab2 = this.props.tab2
     return (
       <div className="water-comes-app-hightlighted">
-          < Row >
-          <div onClick={ tab1 } className="water-comes-app-estimate">
+        <Row>
+          <Col
+            onClick={() => this.toggleTab("skybrud")}
+            className="water-comes-app-estimate"
+          >
             <h3>Skybrud</h3>
-            <img src={riskImageTab1} className="img-fluid" alt="Risiko måler"/>
-            <p>{this.props.riskText}</p>
-          </div>
+            <img src={rain_image} className="img-fluid" alt="Risiko måler" />
+            <p>{this.props.rain_risk.text}</p>
+          </Col>
 
-          <div onClick={ tab2 } className="water-comes-app-estimate">
+          <Col
+            onClick={() => this.toggleTab("stormflod")}
+            className="water-comes-app-estimate"
+          >
             <h3>Stormflod</h3>
-            <img src={riskImageTab2} className="img-fluid" alt="Risiko måler"/>
-            <p>{this.props.riskText_stormflod}</p>
-          </div>
-          </ Row >
+            <img src={riskImageTab2} className="img-fluid" alt="Risiko måler" />
+            <p>{this.props.flood_risk.text}</p>
+          </Col>
+        </Row>
         <div className="water-comes-app-explanation">
-          <h3>Faktorer, der påvirker boligens risiko ved {this.props.type}</h3>
-        { this.props.type === "skybrud" ?
+          <h3>Faktorer, der påvirker boligens risiko ved {this.state.tab}</h3>
+          {this.state.tab === "skybrud" ? (
             <div>
-              {this.getRisks('high')}
-              {this.getRisks('medium')}
-              {this.getRisks('low')}
-              <Risk title={ Risks["other"].title } description={ Risks["other"].description } />
-            </div> :
-           <div>
-            Her skal være tekst om risiko ved stormflod
-
-            <p><b>Højde over vandoverfladen i meter: { this.props.dangers['height'] }</b> <br />
-            Grænser < br/ >
-            Lav: { this.props.dangers['limits']['low'] } < br/>
-            Mellem: { this.props.dangers['limits']['medium'] } < br/>
-            Høj: { this.props.dangers['limits']['high'] } < br/>
-            </p>
-            <img src={ base64str } alt="vandstigning"/ >
-
-
-           </div>
-        }
+              {this.getRisks("high")}
+              {this.getRisks("medium")}
+              {this.getRisks("low")}
+              <Risk
+                title={Risks["other"].title}
+                description={Risks["other"].description}
+              />
+            </div>
+          ) : (
+            <div>
+              Her skal være tekst om risiko ved stormflod
+              <p>
+                Din højde over vandoverfladen i meter: er{" "}
+                {this.props.dangers.flood.groundHeight}.
+              </p>
+            </div>
+          )}
         </div>
-
-      </div>);
+      </div>
+    );
   }
 }
--- src/components/risk.js
+++ src/components/risk.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
+import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
 
 export default class Risk extends Component {
   constructor(props) {
@@ -14,17 +14,27 @@ export default class Risk extends Component {
 
   renderGroup(group) {
     switch (group) {
-      case 'high':
-        return <span className="danger icon-error_outline-24 red">error_outline</span>;
-      case 'medium':
-        return <span className="danger icon-error_outline-24">error_outline</span>;
+      case "high":
+        return (
+          <span className="danger icon-error_outline-24 red">
+            error_outline
+          </span>
+        );
+      case "medium":
+        return (
+          <span className="danger icon-error_outline-24">error_outline</span>
+        );
       default:
-        return <span className="danger icon-check_circle_outline-24">check_circle_outline</span>;
+        return (
+          <span className="danger icon-check_circle_outline-24">
+            check_circle_outline
+          </span>
+        );
     }
   }
 
   render() {
-    var base64str = this.props.image
+    var base64str = this.props.image;
     return (
       <div className="water-comes-app-risk">
         <header onClick={this.toggle}>
@@ -32,27 +42,30 @@ export default class Risk extends Component {
           <h4>{this.props.title}</h4>
           {this.state.showDescription ? (
             <i className="icon-remove-24">remove</i>
-            ) : (
+          ) : (
             <i className="icon-add-24">add</i>
           )}
         </header>
-        {this.state.showDescription ?
-            <Row>
-              <Col>
-                {this.props.description} < br />
-
-                <p><b>{this.props.text} {this.props.percentage}</b></p> < br />
-
-                {
-                  (typeof base64str) == "undefined" ?
-                  "":
-                  <img src={ base64str } alt="img"/ >
-                }
-
-              </Col>
-            </Row>
-          : ''
-          }
+        {this.state.showDescription ? (
+          <Row>
+            <Col>
+              {this.props.description} <br />
+              <p>
+                <b>
+                  {this.props.text} {this.props.percentage}
+                </b>
+              </p>{" "}
+              <br />
+              {typeof base64str == "undefined" ? (
+                ""
+              ) : (
+                <img src={base64str} alt="img" />
+              )}
+            </Col>
+          </Row>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
