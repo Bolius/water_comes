60335bfe702354b719f0a246e65ec60e0bd6323a
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-11-13 11:00:17 +0100

    Moved loading logic to address select

--- src/components/address-select.js
+++ src/components/address-select.js
@@ -1,90 +1,105 @@
-import React from 'react';
-import { Row, Col, Input, Form, Button } from 'reactstrap';
+import React from "react";
+import { Row, Col, Input, Form, Button } from "reactstrap";
+import { BeatLoader as Loader } from "react-spinners";
+
 export default class AdressSelect extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
+      isLoading: false,
       address: "",
       finalAddress: "",
-      dawa: require("dawa-autocomplete2"),
+      dawa: require("dawa-autocomplete2")
     };
   }
 
   handleChange(event) {
     var target = event.target.value;
-    var inputElm = document.getElementById("dawa-autocomplete-input");
     this.setState((prevState, props) => ({
       address: target
     }));
-    var updateRes = this.props.setAddress
-    var setLoading = this.props.setLoading
-    this.state.dawa.dawaAutocomplete(inputElm, { select: function(dawa_res) {
-      setLoading()
-      async function url_to_json(url) {
-        const response = await fetch(url, {mode: 'cors'});
-        let json = await response.json();
-        const bbr_info = await fetch(
-          "https://ml.bolius.dk/bbr/"+ encodeURI(json.adressebetegnelse)
-        )
-        let bbr_json = await bbr_info.json()
-        json["apartment"] = bbr_json.type === "story" || bbr_json.type === "oth";
-        json['has_basement'] = bbr_json.basement_area > 0
-        json['bbr'] = bbr_json
-        json['x'] =json.adgangsadresse.adgangspunkt.koordinater[0]
-        json['y'] =json.adgangsadresse.adgangspunkt.koordinater[1]
-        let resp = await fetch(`https://ml.bolius.dk/waterComes/${json.x}/${json.y}`)
-        //await fetch(`http://127.0.0.1:4000/watercomes/${json.x}/${json.y}`)
-
-        let dangers = await resp.json()
-        console.log("recieved")
-        console.log(dangers)
-        json['dangers'] = dangers
-        return json
-      }
-
-      url_to_json(dawa_res.data.href).then(data => {
-        console.log("data res")
-        console.log(data);
-        let res = {
-          'apartment': data.apartment,
-          'text' : data.adressebetegnelse,
-          'has_basement' : data.has_basement,
-          'bolig': data.adgangsadresse.bebyggelser[0],
-          'bbr': data['bbr'],
-          'x' : data.adgangsadresse.adgangspunkt.koordinater[0],
-          'y' : data.adgangsadresse.adgangspunkt.koordinater[1],
-          'dangers' : data['dangers']
+    var react = this; // Hack to refer to "this" in next function
+    this.state.dawa.dawaAutocomplete(
+      document.getElementById("dawa-autocomplete-input"),
+      {
+        select: function(dawa_res) {
+          react.setState({ isLoading: !react.state.isLoading });
+          async function url_to_json(url) {
+            const response = await fetch(url, { mode: "cors" });
+            let json = await response.json();
+            const bbr_info = await fetch(
+              "https://ml.bolius.dk/bbr/" + encodeURI(json.adressebetegnelse)
+            );
+            let bbr_json = await bbr_info.json();
+            json["apartment"] =
+              bbr_json.type === "story" || bbr_json.type === "oth";
+            json["has_basement"] = bbr_json.basement_area > 0;
+            json["bbr"] = bbr_json;
+            json["x"] = json.adgangsadresse.adgangspunkt.koordinater[0];
+            json["y"] = json.adgangsadresse.adgangspunkt.koordinater[1];
+            let resp = await fetch(
+              `https://ml.bolius.dk/waterComes/${json.x}/${json.y}`
+            );
+            let dangers = await resp.json();
+            json["dangers"] = dangers;
+            return json;
+          }
+          url_to_json(dawa_res.data.href).then(data => {
+            let res = {
+              apartment: data.apartment,
+              text: data.adressebetegnelse,
+              has_basement: data.has_basement,
+              bolig: data.adgangsadresse.bebyggelser[0],
+              bbr: data["bbr"],
+              x: data.adgangsadresse.adgangspunkt.koordinater[0],
+              y: data.adgangsadresse.adgangspunkt.koordinater[1],
+              dangers: data["dangers"]
+            };
+            react.props.setAddress(res);
+          });
         }
-      updateRes(res)
-
-      })
       }
-    });
+    );
   }
 
   render() {
-    return (
-      <div className="water-comes-app-address">
-        <Form onSubmit={(e) => {e.preventDefault();}}>
-          <Row>
-            <Col md={{size: '9'}} sm={'8'}>
-              <div className="autocomplete-container">
-                <Input
-                  type="search"
-                  value={this.state.address}
-                  onChange={this.handleChange}
-                  id="dawa-autocomplete-input"
-                  placeholder="Indtast din adresse...."
-                />
-              </div>
-            </Col>
-            <Col md={{size: '3'}} sm={'4'} className="align-right">
-              <Button color="primary">Tjek risiko</Button>
-            </Col>
-          </Row>
-        </Form>
-      </div>
-    );
+    if (this.state.isLoading) {
+      return (
+        <Loader
+          sizeUnit={"px"}
+          size={25}
+          color={"rgb(94, 179, 219)"}
+          loading={true}
+        />
+      );
+    } else {
+      return (
+        <div className="water-comes-app-address">
+          <Form
+            onSubmit={e => {
+              e.preventDefault();
+            }}
+          >
+            <Row>
+              <Col md={{ size: "9" }} sm={"8"}>
+                <div className="autocomplete-container">
+                  <Input
+                    type="search"
+                    value={this.state.address}
+                    onChange={this.handleChange}
+                    id="dawa-autocomplete-input"
+                    placeholder="Indtast din adresse...."
+                  />
+                </div>
+              </Col>
+              <Col md={{ size: "3" }} sm={"4"} className="align-right">
+                <Button color="primary">Tjek risiko</Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      );
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,67 +1,45 @@
 import React from "react";
 import AdressSelect from "./components/address-select.js";
 import ResultPage from "./views/result-page.js";
-import { BeatLoader as Loader } from "react-spinners";
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.setAddress = this.setAddress.bind(this);
     this.resetAddress = this.resetAddress.bind(this);
-    this.setLoading = this.setLoading.bind(this);
 
     this.state = {
       address: {},
-      address_selected: false,
-      loading: false
+      address_selected: false
     };
   }
 
   setAddress(address) {
-    let state = this.state;
-    state.address = address;
-    state.address_selected = true;
-    state.loading = false;
-    this.setState(state);
+    this.setState({
+      address_selected: true,
+      address: address
+    });
   }
 
   resetAddress() {
     this.setState({ address: {}, address_selected: false });
   }
 
-  setLoading() {
-    this.setState({ loading: !this.state.loading });
-  }
-
   render() {
-    if (this.state.loading) {
+    if (!this.state.address_selected) {
       return (
-        <Loader
-          sizeUnit={"px"}
-          size={25}
-          color={"rgb(94, 179, 219)"}
-          loading={true}
-        />
+        <div>
+          <AdressSelect setAddress={this.setAddress} />
+        </div>
       );
     } else {
-      if (!this.state.address_selected) {
-        return (
-          <div>
-            <AdressSelect
-              setAddress={this.setAddress}
-              setLoading={this.setLoading}
-            />
-          </div>
-        );
-      } else {
-        return (
-          <ResultPage
-            address={this.state.address}
-            reset={this.resetAddress}
-            dangers={this.state.address.dangers}
-          />
-        );
-      }
+      return (
+        <ResultPage
+          address={this.state.address}
+          reset={this.resetAddress}
+          dangers={this.state.address.dangers}
+        />
+      );
     }
   }
 }
