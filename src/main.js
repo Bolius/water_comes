1782351b8e8bc57c69d30261b29ae252eec6b848
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-11-13 10:28:03 +0100

    Cleaned main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import AdressSelect from './components/address-select.js'
-import ResultPage from './views/result-page.js'
-import Modal from 'react-responsive-modal';
-import DataBasis from './components/data-basis.js'
+import React from "react";
+import AdressSelect from "./components/address-select.js";
+import ResultPage from "./views/result-page.js";
+import Modal from "react-responsive-modal";
+import DataBasis from "./components/data-basis.js";
 import { BeatLoader as Loader } from "react-spinners";
-
 
 export default class Main extends React.Component {
   constructor(props) {
@@ -18,7 +17,7 @@ export default class Main extends React.Component {
       address: {},
       address_selected: false,
       showDataDescriber: false,
-      loading: false,
+      loading: false
     };
   }
 
@@ -32,50 +31,60 @@ export default class Main extends React.Component {
 
   toggle() {
     this.setState({
-      showDataDescriber: !this.state.showDataDescriber,
+      showDataDescriber: !this.state.showDataDescriber
     });
   }
 
-  resetAddress(){
-    this.setState({address: {}, address_selected: false})
+  resetAddress() {
+    this.setState({ address: {}, address_selected: false });
   }
 
-  setLoading(){
-    this.setState({ loading: !this.state.loading })
+  setLoading() {
+    this.setState({ loading: !this.state.loading });
   }
 
-  render() {return (
-    <div>
-      { !this.state.loading ?
-        (!this.state.address_selected ?
-          <AdressSelect setAddress={this.setAddress} setLoading={this.setLoading}/>
-        :
-          <ResultPage address={this.state.address} reset={this.resetAddress} dangers={this.state.address.dangers}/>
-        ) :
+  render() {
+    if (this.state.loading) {
+      return (
         <Loader
           sizeUnit={"px"}
           size={25}
           color={"rgb(94, 179, 219)"}
           loading={true}
         />
+      );
+    } else {
+      if (!this.state.address_selected) {
+        return (
+          <div>
+            <AdressSelect
+              setAddress={this.setAddress}
+              setLoading={this.setLoading}
+            />
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <ResultPage
+              address={this.state.address}
+              reset={this.resetAddress}
+              dangers={this.state.address.dangers}
+            />
+            <Modal
+              open={this.state.showDataDescriber}
+              closeOnEsc
+              onClose={this.toggle}
+            >
+              <DataBasis />
+            </Modal>
+
+            <h2 onClick={this.toggle}>
+              <h3>Se datagrundlaget her</h3>
+            </h2>
+          </div>
+        );
       }
-
-      {!this.state.address_selected ?
-        ""
-      :
-      <div>
-      <Modal
-        open={this.state.showDataDescriber}
-        closeOnEsc onClose={this.toggle}>
-        <DataBasis/>
-      </Modal>
-
-      <header onClick={this.toggle}>
-        <h3>Se datagrundlaget her</h3>
-      </header>
-      </div>
-      }
-
-    </div>
-  );}
+    }
+  }
 }
