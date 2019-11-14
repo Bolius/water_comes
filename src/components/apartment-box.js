90c7abd9d0ea5d0d72ecdbeb9c9569a4ff2aa09a
Daniela Petrova Keil - Bolius <dke@bolius.dk>
2019-11-14 15:20:21 +0100

    Appartment text change

--- src/components/apartment-box.js
+++ src/components/apartment-box.js
@@ -4,7 +4,7 @@ export default class ApartmentBox extends React.Component {
   render() {
     return (
       <div className="appartmentbox">
-        <h3>Din adresse viser, at du bor i lejlighed.</h3>
+        <h3>OBS: Din adresse viser, at du bor i lejlighed.</h3>
         <p>
           Derfor er risikoberegningen kun gældende for kælderen. Husk stadig at
           lukke dine vinduer og evt. altandør.
