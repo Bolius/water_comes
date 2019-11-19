04f2a5ef98753904aeff62fd4581b6f183ef84e3
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-11-19 16:52:45 +0100

    Updated text

--- src/components/resume.js
+++ src/components/resume.js
@@ -3,24 +3,46 @@ import RisksDB from "../risks.json";
 
 export default class Resume extends React.Component {
   floodResults() {
-    return (
-      <p>
-        Vores offentligt tilgængelige oplysninger indikerer, at din bolig ligger{" "}
-        {this.props.dangers.flood.groundHeight} meter over havets overflade.
-        <p
-          dangerouslySetInnerHTML={{
-            __html: RisksDB.results.flood[this.props.dangers.flood.risk]
-          }}
-        />
-      </p>
-    );
+    let factors = [];
+    factors.push({
+      text: RisksDB.results[this.props.active][this.props.threatLevel],
+      link:
+        this.props.threatLevel !== "low"
+          ? RisksDB.results[this.props.active].links[0]
+          : undefined
+    });
+    if (this.props.threatLevel !== "low") {
+      factors.push({
+        text: "",
+        link: RisksDB.results[this.props.active].links[1]
+      });
+    }
+
+    return factors.map((factor, i) => (
+      <div>
+        <p key={i} dangerouslySetInnerHTML={{ __html: factor.text }} />
+        {factor.link !== undefined ? (
+          <p class="inline-links-in-article">
+            <span class="category orange">Læs også: </span>
+            <a href={factor.link.url} target="_blank">
+              {factor.link.title}
+            </a>
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
+    ));
   }
 
   rainResults() {
     let factors = [];
     factors.push({
       text: RisksDB.results[this.props.active][this.props.threatLevel],
-      link: RisksDB.results[this.props.active].link
+      link:
+        this.props.threatLevel === "low"
+          ? RisksDB.results[this.props.active].link
+          : undefined
     });
 
     if (this.props.dangers.basement.risk === "high") {
@@ -63,46 +85,34 @@ export default class Resume extends React.Component {
         )}
       </div>
     ));
-
-    return factors.map((factor, i) => (
-      <div>
-        <p key={i} dangerouslySetInnerHTML={{ __html: factor }} />
-        {factor.link !== undefined ? (
-          <p class="inline-links-in-article">
-            <span class="category orange">Læs også: </span>
-            <a href={factor.link.url} target="_blank">
-              Link titel
-            </a>
-          </p>
-        ) : (
-          ""
-        )}
-      </div>
-    ));
   }
 
   render() {
     return (
       <div className="water-comes-app-taken">
-        <h3>Resultater</h3>
+        <h3>Det kan du gøre</h3>
         {this.props.active === "skybrud"
           ? this.rainResults()
           : this.floodResults()}
 
-        <p>
-          Når først DMI har varslet skybrud eller stormflod, er det begrænset,
-          hvad du kan gøre. Men du kan hente{" "}
-          <a
-            taget="_blank"
-            href="https://www.bolius.dk/tjekliste-saadan-goer-du-naar-vandet-kommer-udefra-77128/"
-          >
-            vores overskuelige tjekliste
-          </a>{" "}
-          med gode råd for at se, hvad du kan gøre her og nu. Beregnerens
-          vurdering er vejledende. Ønsker du en vurdering, der tager
-          udgangspunkt i alle boligens konkrete forhold, bør du kontakte en
-          fagperson og arrangere en fysisk inspektion.
-        </p>
+        <div style={{ border: "1px solid black" }}>
+          <p>
+            Når først DMI har varslet skybrud eller stormflod, er det begrænset,
+            hvad du kan gøre. Men du kan hente vores overskuelige tjekliste med
+            gode råd for at se, hvad du kan gøre her og nu.
+            <p class="inline-links-in-article">
+              <span class="category orange">Læs også: </span>
+              <a
+                href="https://www.bolius.dk/tjekliste-saadan-goer-du-naar-vandet-kommer-udefra-77128/"
+                target="_blank"
+              >
+                Hent tjeklisten her
+              </a>
+            </p>
+            Beregnerens vurdering er vejledende og kan aldrig erstatte en faglig
+            gennemgang, der omfatter alle din boligs konkrete forhold.
+          </p>
+        </div>
       </div>
     );
   }
