bb0e3417326545e1af897a750c0b4fe2ea152827
Aamer Khan <ahk@bolius.dk>
2019-11-27 13:59:10 +0100

    Now using XMLHttpRequest instead of fetch for IE compability

--- src/components/address-select.js
+++ src/components/address-select.js
@@ -38,6 +38,102 @@ export default class AdressSelect extends React.Component {
     console.log("Fetching data");
     this.setState({ isLoading: true });
 
+    // Remember "this" for async operations 
+    var that = this;
+
+    // Set up our HTTP request
+    var houseData = {
+          failed: false
+        }, 
+        xhr = new XMLHttpRequest(), 
+        xhrQl = new XMLHttpRequest();
+
+    // Setup our listener to process compeleted requests
+    xhr.onreadystatechange = function () {
+      // Only run if the request is complete
+      if (xhr.readyState !== 4) return;
+
+      // Process our return data
+      if (xhr.status >= 200 && xhr.status < 300) {
+        // Succes, get data
+        let data = JSON.parse(xhr.responseText), 
+            kvhx = data.kvhx;
+        
+        // Create and send a POST request
+        xhrQl.open('POST', process.env.REACT_APP_GRAPHQL_URL);
+        xhrQl.setRequestHeader("Content-Type", "application/json");
+        xhrQl.send(constructQuery(kvhx));
+      } 
+      else {
+        // What to do when the request has failed        
+        that.setState({
+          failed: true,
+          isLoading: false,
+          address: "",
+          finalAddress: "",
+          dawa: require("dawa-autocomplete2")
+        });
+        
+        console.log('error', xhr);
+        console.log(Sentry.captureException(xhr));
+        return { failed: true };
+      }
+
+    };
+
+    // Setup our listener to process QL
+    xhrQl.onreadystatechange = function () {
+      // Only run if the request is complete
+      if (xhrQl.readyState !== 4) return;
+
+      // Process our return data
+      if (xhrQl.status >= 200 && xhrQl.status < 300) {
+        // Succes, get data
+        let data = JSON.parse(xhrQl.responseText);
+        houseData = data.data.house;
+
+        backendLog(dawa_res.tekst, "Vandet kommer indtastet");
+        if (houseData.failed) {
+          return;
+        }
+        
+        let result = {
+          isApartment: houseData.bbrInfo.propType === "Etageboliger",
+          text: houseData.bbrInfo.address,
+          dangers: {
+            basement: { risk: houseData.bbrInfo.hasBasement ? "high" : "low" },
+            flood: houseData.waterRisk.flood,
+            hollowing: houseData.waterRisk.hollowing,
+            fastningDegree: houseData.waterRisk.fastningDegree,
+            conductivity: houseData.waterRisk.conductivity
+          }
+        };
+        result.dangers.rain_threat = computeRainRisk(result.dangers);
+    
+        trackEvent({
+          description: "Adresse indtastet",
+          // Gets 2300 københavn part of adrress
+          eventLabel: result.text
+            .split(",")
+            .slice(-1)
+            .pop(),
+          cloudbirstDimension: result.dangers.rain_threat,
+          floodDimension: result.dangers.flood.risk
+        });
+        
+        that.props.setData(result);
+      } 
+      else {                
+        console.log('error', xhrQl); 
+        return { failed: true };
+      }
+    };
+
+    // Create and send a GET request
+    xhr.open('GET', dawa_res.data.href);
+    xhr.send();
+
+    /*
     let houseData = await fetch(dawa_res.data.href)
       .then(resp => resp.json())
       .then(data => data.kvhx)
@@ -65,36 +161,7 @@ export default class AdressSelect extends React.Component {
         console.log(Sentry.captureException(err));
         return { failed: true };
       });
-    backendLog(dawa_res.tekst, "Vandet kommer indtastet");
-    if (houseData.failed) {
-      return;
-    }
-    console.log(houseData);
-
-    let result = await {
-      isApartment: houseData.bbrInfo.propType === "Etageboliger",
-      text: houseData.bbrInfo.address,
-      dangers: {
-        basement: { risk: houseData.bbrInfo.hasBasement ? "high" : "low" },
-        flood: houseData.waterRisk.flood,
-        hollowing: houseData.waterRisk.hollowing,
-        fastningDegree: houseData.waterRisk.fastningDegree,
-        conductivity: houseData.waterRisk.conductivity
-      }
-    };
-    result.dangers.rain_threat = computeRainRisk(result.dangers);
-
-    trackEvent({
-      description: "Adresse indtastet",
-      // Gets 2300 københavn part of adrress
-      eventLabel: result.text
-        .split(",")
-        .slice(-1)
-        .pop(),
-      cloudbirstDimension: result.dangers.rain_threat,
-      floodDimension: result.dangers.flood.risk
-    });
-    this.props.setData(result);
+    */      
   }
 
   handleChange(event) {
