9595115b77ce2d8465dced7d7a7b9cfaceaedf0b
Benjamin <benjamin@rotendahl.dk>
2019-12-12 16:53:37 +0100

    Testing branch (#82)
    * Changed apartment to functional component

--- src/components/risk-describer.js
+++ src/components/risk-describer.js
@@ -1,90 +1,90 @@
 import React from "react";
+import RiskDB from "../risks.json";
+
 import Risk from "./risk.js";
-import RiskFile from "../risks.json";
-import { Row } from "reactstrap";
 
-export default class RiskDescriber extends React.Component {
-  getRisks(risks) {
-    const risk_types = Object.keys(risks);
-    //console.log(risks);
-    const ordered_risks = ["high", "medium", "low"]
-      .map(level =>
-        risk_types.filter(risk_type => risks[risk_type].risk === level)
-      )
-      .reduce((acc, val) => acc.concat(val), [])
-      .map(key => ({ name: key, data: risks[key] }));
+export default function RiskDescriber(props) {
+  const image = require(`../assets/gauges/risk-${props.threatLevel}.png`);
+  const hacky_threat_nr = // Update Css to avoid this
+    ["low", "medium", "high"].indexOf(props.threatLevel) + 2;
 
-    return ordered_risks.map((threat, i) => (
-      <Risk
-        key={i}
-        tab={this.props.active}
-        threat={threat.data.risk}
-        title={RiskFile[threat.name][threat.data.risk]}
-        description={RiskFile[threat.name].description}
-        image={this.formatImage(threat.data.image)}
-        dangers={this.props.dangers}
-      />
-    ));
-  }
+  const riskImage = (
+    <div className="water-comes-app-estimate">
+      <img src={image} className="img-fluid" alt="Risiko måler" />
+      <p className={`risc-${hacky_threat_nr}`}>
+        {getRiskText(props.threatLevel)}
+      </p>
+    </div>
+  );
 
-  formatImage(image) {
-    if (image !== undefined) {
-      image = image.substring(2, image.length - 1);
-      image = `data:image/png;base64,${image}`;
-    }
-    return image;
-  }
+  return (
+    <div className="water-comes-app-hightlighted">
+      {riskImage}
 
-  render() {
-    let riskText = "";
-    switch (this.props.threatLevel) {
-      case "high":
-        riskText = "Høj risiko";
-        break;
-      case "medium":
-        riskText = "Mellem risiko";
-        break;
-      case "low":
-        riskText = "lav risiko";
-        break;
-      default:
-        console.log("error");
-    }
-    const image = require(`../assets/gauges/risk-${this.props.threatLevel}.png`);
-    const hacky_threat_nr =
-      ["low", "medium", "high"].indexOf(this.props.threatLevel) + 2;
-    // Update Css to avoid this
+      <div className="water-comes-app-explanation">
+        <h3>Faktorer, der påvirker boligens risiko ved {props.floodType}</h3>
+        <div className="risk-list">
+          {constructRisks(props.risks, props.logClick)}
 
-    return (
-      <div className="water-comes-app-hightlighted">
-        <Row noGutters className="water-comes-app-estimate">
-          <img src={image} className="img-fluid" alt="Risiko måler" />
-          <p className={`risc-${hacky_threat_nr}`}>{riskText}</p>
-        </Row>
-        <div className="water-comes-app-explanation">
-          <h3>
-            Faktorer, der påvirker boligens risiko ved {this.props.active}
-          </h3>
-          <div>
-            {this.getRisks(this.props.risks)}
-            {this.props.active === "skybrud" ? (
-              <Risk
-                title={RiskFile["rain_other"].title}
-                description={RiskFile["rain_other"].description}
-                threat={"medium"}
-                dangers={this.props.dangers}
-              />
-            ) : (
-              <Risk
-                title={RiskFile["flood_other"].title}
-                description={RiskFile["flood_other"].description}
-                threat={"medium"}
-                dangers={this.props.dangers}
-              />
-            )}
-          </div>
+          {props.floodType === "skybrud" ? (
+            <Risk
+              title={RiskDB["rain_other"].title}
+              description={RiskDB["rain_other"].description}
+              threatLevel={"medium"}
+              logClick={() => props.logClick(RiskDB["rain_other"].title)}
+            />
+          ) : (
+            <Risk
+              title={RiskDB["flood_other"].title}
+              description={RiskDB["flood_other"].description}
+              threatLevel={"medium"}
+              logClick={() => props.logClick(RiskDB["flood_other"].title)}
+            />
+          )}
         </div>
       </div>
-    );
+    </div>
+  );
+}
+
+function getRiskText(threatLevel) {
+  switch (threatLevel) {
+    case "high":
+      return "Høj risiko";
+    case "medium":
+      return "Mellem risiko";
+    case "low":
+      return "lav risiko";
+    default:
+      throw new `Invalid threatlevel: ${threatLevel}`();
   }
+}
+
+function constructRisks(risks, logClick) {
+  const risk_types = Object.keys(risks);
+  const ordered_risks = ["high", "medium", "low"]
+    .map(level =>
+      risk_types.filter(risk_type => risks[risk_type].risk === level)
+    )
+    .reduce((acc, val) => acc.concat(val), [])
+    .map(key => ({ name: key, data: risks[key] }));
+
+  return ordered_risks.map((threat, i) => (
+    <Risk
+      key={i}
+      logClick={() => logClick(RiskDB[threat.name][threat.data.risk])}
+      threatLevel={threat.data.risk}
+      title={RiskDB[threat.name][threat.data.risk]}
+      description={RiskDB[threat.name].description}
+      map={formatImage(threat.data.image)}
+    />
+  ));
+}
+
+function formatImage(image) {
+  if (image !== undefined) {
+    image = image.substring(2, image.length - 1);
+    image = `data:image/png;base64,${image}`;
+  }
+  return image;
 }
