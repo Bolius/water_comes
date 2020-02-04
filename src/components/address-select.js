fda064e8358f9c5fd3b6cb6dc11b6c5a84663b2c
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2020-02-05 00:22:10 +0100

    Done with switch to lambda

--- src/components/address-select.js
+++ src/components/address-select.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Row, Col, Input, Form, Button } from "reactstrap";
 import { BeatLoader as Loader } from "react-spinners";
 import * as dawaModule from "dawa-autocomplete2";
-import axios from "axios";
 import Modal from "react-responsive-modal";
 import getFloodData from "../data-handlers/get-flood-data.js";
 
@@ -11,9 +10,9 @@ export default function AdressSelect(props) {
   const [dataFailed, setDataFailed] = useState(false);
   const [inputAddress, setInputAddress] = useState("");
   const [dawa, setDawa] = useState(dawaModule);
-  let handle_dawa_resp = dawa_resp => {
+  let handle_dawa_resp = bbr_id => {
     setIsLoading(true);
-    getFloodData(dawa_resp.data.id, resp => {
+    getFloodData(bbr_id, resp => {
       if (resp.failed) {
         setDataFailed(true);
         setIsLoading(false);
@@ -30,21 +29,19 @@ export default function AdressSelect(props) {
       dawa.dawaAutocomplete(
         document.getElementById("dawa-autocomplete-input"),
         {
-          select: dawa_res => handle_dawa_resp(dawa_res)
+          select: dawa_resp => handle_dawa_resp(dawa_resp.data.id)
         }
       );
     }
+    // Check if id is set in query param
+    const params = window.location.search.split("&");
+    for (var param of params) {
+      if (param.includes("unadr_bbrid=")) {
+        const unit_bbr = param.split("=")[1];
+        handle_dawa_resp(unit_bbr);
+      }
+    }
   });
-  // // Check if id is set in query param
-  // const params = window.location.search.split("&");
-  // for (var param of params) {
-  //   if (param.includes("unadr_bbrid=")) {
-  //     const unit_bbr = param.split("=")[1];
-  //     axios.get("https://dawa.aws.dk/adresser/" + unit_bbr).then(resp => {
-  //       handle_dawa_resp(resp);
-  //     });
-  //   }
-  // }
 
   return isLoading ? (
     <div className="water-comes-app-address">
