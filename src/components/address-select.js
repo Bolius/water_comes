d5fc00bb92a1fdd637a42948a60ecdec9b47b583
Mikkel Sørensen <mikkel0228@gmail.com>
2021-03-08 11:04:48 +0100

    Ny fejlbesked grundet fejl hos kortfordeleren (#306)
    * Ny fejlbeskrivelse grundet fejl hos kortforsyningen
    
    Signed-off-by: Mikkel Sørensen <mikkel0228@gmail.com>
    
    * Fejlbesked grundet fejl hos kortsyningen
    
    Signed-off-by: Mikkel Sørensen <mikkel0228@gmail.com>

--- src/components/address-select.js
+++ src/components/address-select.js
@@ -131,12 +131,16 @@ export default function AdressSelect(props) {
       <Modal open={dataFailed} closeOnEsc onClose={() => setDataFailed(false)}>
         <Col className="water-comes-app-data">
           <p>
-            Der opstod en fejl under beregningen. Prøv igen ved at genopfriske
-            siden. Virker det ikke, kan fejlen skyldes et teknisk problem hos en
-            af de leverandører, vi henter oplysninger fra.
+            Fredag d. 5. marts:<br />
+            Det er desværre ikke muligt at bruge beregneren i øjeblikket, da vi ikke kan få de nødvendige data fra leverandøren.
           </p>
         </Col>
       </Modal>
     </div>
   );
 }
+
+// Normalt indhol af "water-comes-app-data":
+// Der opstod en fejl under beregningen. Prøv igen ved at genopfriske
+// siden. Virker det ikke, kan fejlen skyldes et teknisk problem hos en
+// af de leverandører, vi henter oplysninger fra.
