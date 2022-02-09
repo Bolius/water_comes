7bb9a0765ffbe19da6ae8eac80e5f1a10070b696
Mikkel Sørensen <mikkel0228@gmail.com>
2022-02-09 13:56:58 +0100

    Replaced satelite photo from Google Maps with Datafordeler
    Signed-off-by: Mikkel Sørensen <mikkel0228@gmail.com>

--- src/components/map-box.js
+++ src/components/map-box.js
@@ -3,29 +3,66 @@ import { Row, Col, Button } from "reactstrap";
 import ApartmentBox from "./apartment-box.js";
 
 export default class MapBox extends React.Component {
-  render() {
-    const mapParams = {
-      center: this.props.address,
-      zoom: 18,
-      size: "600x338",
-      maptype: "hybrid",
-      markers: `color:0x58B1DD|location:${this.props.address}`,
-      key: process.env.REACT_APP_GOOGLE_MAPS_KEY,
-      scale: 1,
-      language: "danish",
-      region: "dk"
-    };
-    let mapUrl = "https://maps.googleapis.com/maps/api/staticmap?";
-    for (let [key, value] of Object.entries(mapParams)) {
-      mapUrl += `${key}=${encodeURI(value)}&`;
-    }
-    mapUrl = mapUrl.substring(0, mapUrl.length - 1);
+  constructor(props) {
+    super(props)
+    this.state = {mapUrl: ""}
+  }
+
+  componentDidMount() {
+    // Params for image rendering
+    const boxSizeX = 58
+    const boxSizeY = 37
+    const scalar = 1.5
+
+    // Fetch coordinates of address
+    fetch('https://api.dataforsyningen.dk/adresser?q=' + this.props.address + '&struktur=mini&srid=25832')
+      .then(response => response.json())
+      .then(data => {
+        // Create bounding box for address
+        let x = data[0]["x"]
+        let y = data[0]["y"]
+    
+        let minX = x - boxSizeX / scalar
+        let minY = y - boxSizeY / scalar
+        let maxX = x + boxSizeX / scalar
+        let maxY = y + boxSizeY / scalar
+        let bbox = [minX,minY,maxX,maxY].toString();
+        
+        // Fetch actual satelite image
+        const mapParams = {
+          username: process.env.REACT_APP_DATAFORDELER_USER,
+          password: process.env.REACT_APP_DATAFORDELER_PASS,
+          SERVICE: "WMS",
+          VERSION: "1.3.0",
+          REQUEST: "GetMap",
+          BBOX: bbox,
+          CRS: "EPSG:25832",
+          WIDTH:580,
+          HEIGHT: 370,
+          FORMAT: "image/jpeg",
+          DPI: 96,
+          MAP_RESOLUTION: 96,
+          FORMAT_OPTIONS: "dpi:96",
+          Layers: "orto_foraar",
+          styles: ""
+        }
+        
+        var mapUrl = "https://services.datafordeler.dk/GeoDanmarkOrto/orto_foraar/1.0.0/WMS?";
+        for (let [key, value] of Object.entries(mapParams)) {
+          mapUrl += `${key}=${encodeURI(value)}&`;
+        }
+        mapUrl = mapUrl.substring(0, mapUrl.length - 1);
+        this.setState({mapUrl})
+      });
+  }
+
+  render() { 
     return (
       <div className="water-comes-app-resume">
         <h2>Risikovurdering af din bolig</h2>
         <Row>
           <Col sm={{ size: "6" }}>
-            <img src={mapUrl} alt="Bolig Kort" className="map-image" />
+            <img src={this.state.mapUrl} alt="Bolig Kort" className="map-image" />
             <address>{this.props.address}</address>
             <Button color="link" onClick={this.props.reset}>
               Skift adresse
